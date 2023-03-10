15625982559c05978296317539596e524fcb4980
MKA <67032796+MKA54@users.noreply.github.com>
2023-03-10 09:54:57 +0700

    Add files via upload

---
+++ PlumsailWeb/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+    <nav>
+        <p>
+            <router-link to="/">
+                Form
+            </router-link>
+            <router-link to="/forms">
+                All form
+            </router-link>
+        </p> 
+    </nav>
+    <router-view />
+</template>
+
+<script>
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
